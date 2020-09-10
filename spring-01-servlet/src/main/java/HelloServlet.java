e9dc87363d5bac435c2c6aa38f6d2bb65b6e2c37
L1325694687 <1325694687@qq.com>
2020-09-10 15:45:00 +0800

    hello

--- spring-01-servlet/src/main/java/HelloServlet.java
+++ spring-01-servlet/src/main/java/HelloServlet.java
@@ -1,2 +1,33 @@
-public class HelloServlet {
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取前端参数
+        String method =  req.getParameter("method");
+        if(method.equals("add")){
+            req.getSession().setAttribute("msg","执行了add方法");
+        }
+        if (method.equals("delete")){
+            req.getSession().setAttribute("msg","执行了delete方法");
+        }
+
+        //调用业务层
+
+        //视图转发或者重定向
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
